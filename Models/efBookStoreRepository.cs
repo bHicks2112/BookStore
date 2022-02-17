5b11d218f3e69e98508997883ea4c3bd7455fbaa
bHicks2112 <79944750+bHicks2112@users.noreply.github.com>
2022-02-16 17:49:59 -0700

    Add project files.

---
+++ Models/efBookStoreRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class efBookStoreRepository : iBookStoreRepository
+    {
+
+        private BookstoreContext context { get; set; }
+
+        public efBookStoreRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Books> Books => context.Books;
+    }
+}
