9a1cb20ffbd0253296764192f2b0269ffbe8b43d
bHicks2112 <79944750+bHicks2112@users.noreply.github.com>
2022-03-16 23:13:31 -0600

    mission11

---
+++ Models/AppIdentityDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Models
+{
+    public class AppIdentityDBContext : IdentityDbContext<IdentityUser>
+    {
+        public AppIdentityDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,6 +33,12 @@ namespace BookStore
            {
                options.UseSqlite(Configuration["ConnectionStrings:BookstoreDBConnection"]);
            });
+
+            services.AddDbContext<AppIdentityDBContext>(options =>
+               options.UseSqlite(Configuration["ConnectionStrings:IdentityConnection"]));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDBContext>();
 
             services.AddScoped<iBookStoreRepository, efBookStoreRepository>();
             services.AddScoped<ICheckOutRepository, EFCheckOutRepository>();
@@ -65,6 +72,8 @@ namespace BookStore
             app.UseStaticFiles();
             app.UseSession();
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
